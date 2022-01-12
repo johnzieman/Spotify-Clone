955b815e5cf1a3b7a26a5c3eba82cee5cf1efb9e
johnzieman <j.g.zokirov@gmail.com>
2022-01-12 13:30:04 +0500

    initial commit

--- app/src/main/java/com/lockwoodpublishing/telegram_clone/MainActivity.kt
+++ app/src/main/java/com/lockwoodpublishing/telegram_clone/MainActivity.kt
@@ -2,10 +2,14 @@ package com.lockwoodpublishing.telegram_clone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lockwoodpublishing.telegram_clone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mBinding.root)
     }
 }
